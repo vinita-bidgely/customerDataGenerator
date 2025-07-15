61e0f0efb5fbc91890769ebfe98f9cde683cd442
vinita-bidgely <vinitapal@bidgely.com>
2025-07-15 12:19:37 +0530

    Initial Commit

---
+++ src/main/java/com/bidgely/customerDataGenerator/CustomerDataGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.bidgely.customerDataGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerDataGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerDataGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bidgely/customerDataGenerator/CustomerDataGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bidgely.customerDataGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerDataGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
